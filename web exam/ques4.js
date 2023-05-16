047bc865e8962b7756e7f66235acc9fbe6855d2b
aditya-sonavane-05 <129821631+aditya-sonavane-05@users.noreply.github.com>
2023-05-17 00:28:01 +0530

    Add files via upload

--- web exam/ques4.js
+++ web exam/ques4.js
@@ -1,16 +1,29 @@
-function countDigitsAndSum(number) {
-    let digits = number.toString().split('');
-    let sum = 0;
-  
-    for (let digit of digits) {
-      sum += parseInt(digit);
-    }
-  
-    return {
-      digitCount: digits.length,
-      digitSum: sum
-    };
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET' && req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(`
+      <html>
+        <head>
+          <title>Example form</title>
+        </head>
+        <body>
+          <h1>Example form</h1>
+          <form method="POST" action="/">
+            <label for="name">Name:</label>
+            <input type="text" name="name"><br>
+            <label for="email">Email:</label>
+            <input type="email" name="email"><br>
+            <input type="submit" value="Submit">
+          </form>
+        </body>
+      </html>
+    `);
+    res.end();
   }
-  
-  console.log(countDigitsAndSum(234123)); // Output: { digitCount: 6, digitSum: 15 }
-  
+});
+
+server.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
