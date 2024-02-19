c9d27086594250589abdcff7fb8c0aca06cb3308
aditya-sonavane-05 <129821631+aditya-sonavane-05@users.noreply.github.com>
2024-02-19 22:26:29 +0530

    Delete web exam directory

--- web exam/expresss1/java2.js
+++
@@ -1,3 +0,0 @@
-let array = [3, 1, 5, 2, 4];
-array.sort();
-console.log(array);
--- web exam/expresss1/java6.js
+++
@@ -1,7 +0,0 @@
-function isFirstCharacterUppercase(str) {
-    return str[0] === str[0].toUpperCase();
-  }
-  
-  console.log(isFirstCharacterUppercase('Hello')); // Output: true
-  console.log(isFirstCharacterUppercase('world')); // Output: false
-  
